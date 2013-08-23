af182a9a21915f55348e33e4507cf4fb275d3970
psmits <thestinkypete@gmail.com>
2013-08-23 14:05:54 -0500

    all correlation tests done

--- R/trait_analysis.r
+++ R/trait_analysis.r
@@ -1,4 +1,5 @@
 library(plyr)
+library(reshape2)
 
 source('../R/diet_dynamics.r')
 source('../R/life_dynamics.r')
@@ -37,3 +38,26 @@ loco.cor <- lapply(lfwin.bg, function(x) {
 
 # diet ~ locomotor
 # compare like with like
+# there are more locomotor categories than diet categories
+io <- list()
+for(ii in seq(length(dtwin.bg))) {
+  dit <- dtwin.bg[[ii]]
+  out <- list()
+#  lapply(dit, function(x) print(length(x)))
+  for(jj in seq(length(lfwin.bg))) {
+    ll <- lfwin.bg[[jj]]
+#    lapply(ll, function(x) print(length(x)))
+    out[[jj]] <- mapply(function(x, y) {
+                        x <- unlist(x)
+                        y <- unlist(y)
+                        xx <- x[names(x) %in% names(y)]
+                        yy <- y[names(y) %in% names(x)]
+                        try(cor.test(diff(rev(xx)), diff(rev(yy)), 
+                                     method = 'spearman', 
+                                     exact = FALSE))
+                   }, x = dit, y = ll, SIMPLIFY = FALSE)
+  }
+  names(out) <- names(lfwin.bg)
+  io[[ii]] <- out
+}
+names(io) <- names(dtwin.bg)
