daf30c09b530536ad8ee24caae1202b4bfc1bc70
psmits <thestinkypete@gmail.com>
2014-02-24 21:49:17 -0600

    fixed censoring problem

--- automate.sh
+++ automate.sh
@@ -2,9 +2,9 @@
 
 cd ./R
 
-nohup nice R CMD BATCH --vanilla ../R/time_plots.r
-nohup nice R CMD BATCH --vanilla ../R/surv_plots.r
-nohup nice R CMD BATCH --vanilla ../R/surv_tables.r
+nohup nice R CMD BATCH --vanilla ../R/time_plots.r &
+nohup nice R CMD BATCH --vanilla ../R/surv_plots.r &
+nohup nice R CMD BATCH --vanilla ../R/surv_tables.r &
 
 cd ..
 
