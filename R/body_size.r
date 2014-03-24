749bad45a70ddd740e4f7c0e9f5649ecc5954883
psmits <thestinkypete@gmail.com>
2014-03-24 14:37:31 -0500

    increased body mass sampling

--- R/body_size.r
+++ R/body_size.r
@@ -1,5 +1,6 @@
 library(stringr)
 library(plyr)
+source('../R/predict_mass.r')
 
 source('../R/taxon_names.r')
 source('../R/na_mung.r')
@@ -10,6 +11,22 @@ pbdb <- read.csv('../data/mam_avg_measure.txt', stringsAsFactors = FALSE)
 now <- read.csv('../data/now_database.csv', stringsAsFactors = FALSE)
 pan <- read.delim('../data/pantheria.txt', stringsAsFactors = FALSE)
 tomiya <- read.csv('../data/tomiya_mass.csv', stringsAsFactors = FALSE)
+
+# play with the pbdb measures
+#first.match <- str_detect(pbdb$position, 'm1')
+#problem.dash <- str_detect(pbdb$position, '\\-')
+#good <- first.match & !problem.dash
+#pbdb$position[first.match & !problem.dash]
+#simple solution
+tooth.spec <- pbdb$species[(pbdb$position == 'm1' | pbdb$position == 'Lower m1') 
+                           & pbdb$measurement == 'area']
+mass.spec <- pbdb$species[pbdb$measurement == 'mass']
+update.mass <- tooth.spec[!(tooth.spec %in% mass.spec)]
+update.block <- pbdb[pbdb$species %in% update.mass, ]
+update.block <- update.block[update.block$measurement == 'area', ]
+tooth.mean <- ddply(update.block, .(species), summarize,
+                    mean(as.numeric(mean)))
+tooth.mean$mass <- predmass(tooth.mean[, 2])
 
 pbdb <- pbdb[pbdb$measurement == 'mass', ]
 
@@ -44,8 +61,13 @@ tomiya$Taxon <- str_replace(tomiya$Taxon, '_', ' ')
 na.tom <- get.val(tomiya$Taxon, dat$name.bi, exp(tomiya$LnMass))
 er.tom <- get.val(tomiya$Taxon, eur$name.bi, exp(tomiya$LnMass))
 
-na.mass <- rbind(na.pbdb, na.now, na.pan, na.tom)
-er.mass <- rbind(na.pbdb, er.now, er.pan, er.tom)
+# my predictions from above
+na.me <- get.val(tooth.mean[, 1], dat$name.bi, tooth.mean$mass)
+er.me <- get.val(tooth.mean[, 1], eur$name.bi, tooth.mean$mass)
+
+na.mass <- rbind(na.pbdb, na.now, na.pan, na.tom, na.me)
+er.mass <- rbind(er.pbdb, er.now, er.pan, er.tom, er.me)
+
 
 # clear out duplicates
 na.mass <- na.mass[!duplicated(na.mass[, 1]), ]
