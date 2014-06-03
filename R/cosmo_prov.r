d0971209424471ef3f94834ace5fcedcba7a8e99
psmits <thestinkypete@gmail.com>
2014-06-03 13:48:19 -0500

    generic level networks

--- R/cosmo_prov.r
+++ R/cosmo_prov.r
@@ -36,13 +36,16 @@ eurwin.bg <- biogeo(eurwin)  # europe
 naocc.mat <- unique(dat[, c('occurrence.genus_name', 'bins', 'gid')])
 nagenwin <- network.bin(naocc.mat, bin = 'bins', 
                         taxa = 'occurrence.genus_name', loc = 'gid')
+nagen.bg <- biogeo(nagenwin) # generic na
+
 erocc.mat <- unique(eur[, c('occurrence.genus_name', 'bins', 'gid')])
 ergenwin <- network.bin(erocc.mat, bin = 'bins', 
                         taxa = 'occurrence.genus_name', loc = 'gid')
+ergen.bg <- biogeo(ergenwin) # generic europe
 
 
 # need to do this for traits
-biogeo.trait <- function(taxawin, dat) {
+biogeo.trait <- function(taxawin, dat, bin = 'bins', taxa, loc = 'gid') {
   biocom <- lapply(taxawin, infomap.community)
   biomes <- Map(contract.vertices, taxawin, lapply(biocom, membership))
   biomes <- lapply(biomes, function(x){
@@ -53,15 +56,15 @@ biogeo.trait <- function(taxawin, dat) {
 
   diet <- split(dat, f = dat$comdiet)
   dietwin <- lapply(diet, function(x) {
-                    network.bin(x, bin = 'bins', taxa = 'name.bi', loc = 'gid')})
+                    network.bin(x, bin = bin, taxa = taxa, loc = loc)})
   life <- split(dat, f = dat$comlife)
   lifewin <- lapply(life, function(x) {
-                    network.bin(x, bin = 'bins', taxa = 'name.bi', loc = 'gid')})
+                    network.bin(x, bin = bin, taxa = taxa, loc = loc)})
 
   trait <- lapply(taxawin, function(x) {
-                  mm <- dat$name.bi %in% V(x)$name
-                  unique(dat[mm, c('name.bi', 'comdiet', 'comlife')])})
-  trait <- lapply(trait, function(x) x[!(duplicated(x$name.bi)), ])
+                  mm <- dat[, taxa] %in% V(x)$name
+                  unique(dat[mm, c(taxa, 'comdiet', 'comlife')])})
+  trait <- lapply(trait, function(x) x[!(duplicated(x[, taxa])), ])
   dtwin.bg <- list(bc = lapply(dietwin, function(x) lapply(x, bc)),
                    avgcoc = Map(avgocc, graph = taxawin, 
                                 membership = mem, 
@@ -83,5 +86,25 @@ biogeo.trait <- function(taxawin, dat) {
   out
 }
 
-na.trait <- biogeo.trait(taxawin, dat)  # north america
-er.trait <- biogeo.trait(eurwin, eur)  # europe
+na.trait <- biogeo.trait(taxawin, dat, bin = 'bins', 
+                         taxa = 'name.bi', loc = 'gid')  # north america
+er.trait <- biogeo.trait(eurwin, eur, bin = 'bins',
+                         taxa = 'name.bi', loc = 'gid')  # europe
+
+# generic level
+gnat <- ddply(dat, .(occurrence.genus_name), summarize,
+              comdiet = names(which.max(table(comdiet))),
+              comlife = names(which.max(table(comlife))))
+gnat <- gnat[match(naocc.mat[, 1], gnat[, 1]), ]
+gnaocc.mat <- cbind(naocc.mat, gnat[, 2:3])
+gna.trait <- biogeo.trait(nagenwin, gnaocc.mat, bin = 'bins', 
+                          taxa = 'occurrence.genus_name', loc = 'gid')
+
+gert <- ddply(eur, .(occurrence.genus_name), summarize,
+              diet = names(which.max(table(comdiet))),
+              move = names(which.max(table(comlife))))
+gert <- gnat[match(erocc.mat[, 1], gert[, 1]), ]
+gerocc.mat <- cbind(erocc.mat, gert[, 2:3])
+ger.trait <- biogeo.trait(ergenwin, gerocc.mat, bin = 'bins', 
+                          taxa = 'occurrence.genus_name', loc = 'gid')
+
