2ca9cb5d0e0b1ab45c5a2b3cd1cf2cafc490d9a4
psmits <thestinkypete@gmail.com>
2015-02-05 15:47:28 -0600

    started messing around with combining all the phylogenies. run over night/on geomips

--- R/phylogeny.r
+++ R/phylogeny.r
@@ -2,66 +2,18 @@ library(plyr)
 library(phytools)
 library(stringr)
 library(paleotree)
+library(phangorn)
 
 load('../data/update_taxonomy.rdata')
 source('../R/phylo_gen.r')
 source('../R/na_mung.r')
-load('../data/many_trees.rdata')
+#load('../data/many_trees.rdata')
 
 raia.tree <- read.tree('../data/raia_tree.txt')
 tom.tree <- read.nexus('../data/tomiya_tree.nex')
 
-to.genera <- function(tree) {
-  #  from liam revell
-  tips<-tree$tip.label
-  genera<-unique(sapply(strsplit(tips,"_"),function(x) x[1]))
-  ## here are our genera
-  ## now drop all but one of each
-  ii<-sapply(genera,function(x,y) grep(x,y)[1],y=tips)
-  tree<-drop.tip(tree,setdiff(tree$tip.label,tips[ii]))
-  tree$tip.label<-sapply(strsplit(tree$tip.label,"_"),function(x) x[1])
-  tree
-}
-
-
-not.clean <- laply(lapply(mam.flat, function(x) x$tip.label), is.bad)
-
-to.clean <- mam.flat[not.clean]
-
-scrub <- function(tree) {
-  tree <- collapse.singles(tree)
-
-  if(any(is.na(tree$tip.label)) | any(tree$tip.label == '')) return(NULL)
-
-  tester <- drop.tip(tree, which(str_detect(tree$tip.label, '\\.')))
-  if(sum(str_detect(tester$tip.label, '_')) == 1) {
-    return(NULL)
-  } else if (is.null(tester)) { 
-    return(NULL)
-  } else {
-    tester <- drop.tip(tester, which(!str_detect(tester$tip.label, '_')))
-  }
-
-  if(!is.null(tester)) {
-     tester$tip.label <- str_replace(tester$tip.label, '^([^_]*_[^_]*)_.*$', '\\1')
-  }
-  tester
-}
-
-clean.out <- llply(to.clean, scrub)
-
-class(mam.flat) <- 'NULL'
-mam.flat[not.clean] <- clean.out
-rmd <- which(laply(mam.flat, is.null))
-
-final.trees <- mam.flat[-rmd]
-no.extinct <- laply(final.trees, function(x) {
-                    nar <- clean.taxon(x$tip.label)
-                    o <- nar %in% unique(dat$name.bi)
-                    any(o)})
-
-my.trees <- final.trees[no.extinct]
-my.trees[[length(my.trees) + 1]] <- raia.tree
+fam.tree <- read.nexus('../data/meredith.nex')
+super.tree <- read.nexus('../data/bininda_emonds.nex')
 
 # make taxonomy tree of what i have
 # north america
@@ -98,27 +50,77 @@ lad <- lad[match(na.tree$tip.label, lad[, 1]), ]
 datmat <- cbind(fad[, 2], lad[, 2])
 rownames(datmat) <- fad[, 1]
 
-na.tree <- timeLadderTree(na.tree, timeData = datmat)
-na.scale <- timePaleoPhy(na.tree, timeData = datmat, 
-                         type = 'mbl', vartime = 0.1)
-save(na.scale, file = '../data/taxonomy_tree.rdata')
+#na.tree <- timeLadderTree(na.tree, timeData = datmat)
+#na.scale <- timePaleoPhy(na.tree, timeData = datmat, 
+#                         type = 'mbl', vartime = 0.1)
+#save(na.scale, file = '../data/taxonomy_tree.rdata')
 
-my.trees[[length(my.trees) + 1]] <- na.tree
+# raia.tree is species level
+# super tree is species level
+species.trees <- c(raia.tree, super.tree[[1]], na.tree)
+class(species.trees) <- 'multiPhylo'
+big.tree <- mrp.supertree(species.trees)
 
-genera.trees <- list()
-for(ii in seq(length(my.trees))) {
-  genera.trees[[ii]] <- try(to.genera(my.trees[[ii]]))
-}
-genera.trees <- genera.trees[laply(genera.trees, 
-                                   function(x) class(x) != 'try-error')]
-genera.trees[[length(genera.trees) + 1]] <- tom.tree
 
-biggest <- order(laply(genera.trees, function(x) length(x$tip.label)))
-big.genera <- tail(genera.trees[biggest], 10)
 
-class(my.trees) <- 'multiPhylo'
-class(genera.trees) <- 'multiPhylo'
-class(big.genera) <- 'multiPhylo'
-#my.super <- mrp.supertree(my.trees)
-#genera.super <- mrp.supertree(genera.trees)
-#big.super <- mrp.supertree(big.genera)
+
+#to.genera <- function(tree) {
+#  #  from liam revell
+#  tips<-tree$tip.label
+#  genera<-unique(sapply(strsplit(tips,"_"),function(x) x[1]))
+#  ## here are our genera
+#  ## now drop all but one of each
+#  ii<-sapply(genera,function(x,y) grep(x,y)[1],y=tips)
+#  tree<-drop.tip(tree,setdiff(tree$tip.label,tips[ii]))
+#  tree$tip.label<-sapply(strsplit(tree$tip.label,"_"),function(x) x[1])
+#  tree
+#}
+#
+#not.clean <- laply(lapply(mam.flat, function(x) x$tip.label), is.bad)
+#to.clean <- mam.flat[not.clean]
+#
+#scrub <- function(tree) {
+#  tree <- collapse.singles(tree)
+#  if(any(is.na(tree$tip.label)) | any(tree$tip.label == '')) return(NULL)
+#  tester <- drop.tip(tree, which(str_detect(tree$tip.label, '\\.')))
+#  if(sum(str_detect(tester$tip.label, '_')) == 1) {
+#    return(NULL)
+#  } else if (is.null(tester)) { 
+#    return(NULL)
+#  } else {
+#    tester <- drop.tip(tester, which(!str_detect(tester$tip.label, '_')))
+#  }
+#  if(!is.null(tester)) {
+#     tester$tip.label <- str_replace(tester$tip.label, '^([^_]*_[^_]*)_.*$', '\\1')
+#  }
+#  tester
+#}
+#
+#clean.out <- llply(to.clean, scrub)
+#class(mam.flat) <- 'NULL'
+#mam.flat[not.clean] <- clean.out
+#rmd <- which(laply(mam.flat, is.null))
+#final.trees <- mam.flat[-rmd]
+#no.extinct <- laply(final.trees, function(x) {
+#                    nar <- clean.taxon(x$tip.label)
+#                    o <- nar %in% unique(dat$name.bi)
+#                    any(o)})
+#
+#my.trees <- final.trees[no.extinct]
+#my.trees[[length(my.trees) + 1]] <- raia.tree
+#
+#
+#my.trees[[length(my.trees) + 1]] <- na.tree
+#
+#genera.trees <- list()
+#for(ii in seq(length(my.trees))) {
+#  genera.trees[[ii]] <- try(to.genera(my.trees[[ii]]))
+#}
+#genera.trees <- genera.trees[laply(genera.trees, 
+#                                   function(x) class(x) != 'try-error')]
+#genera.trees[[length(genera.trees) + 1]] <- tom.tree
+#
+#class(my.trees) <- 'multiPhylo'
+#class(genera.trees) <- 'multiPhylo'
+##my.super <- mrp.supertree(my.trees)
+##genera.super <- mrp.supertree(genera.trees)
