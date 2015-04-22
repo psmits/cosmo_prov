2e5d536a5de7af8ce85f6e30fe1a70ff56aed6ff
psmits <thestinkypete@gmail.com>
2015-04-22 14:32:06 -0500

    moved gaan talk out of here.

--- src/spatial_model_wrap.sh
+++ src/spatial_model_wrap.sh
@@ -12,22 +12,22 @@ do
       output file=../data/meta_dump/meta_sample/spatial_samp_${n}_${i}.csv &
   done
   wait
-  for i in `seq 1 4`;
-  do
-    # this is the poisson model
-    ./spatial_zip sample num_samples=5000 num_warmup=5000 random seed=420 \
-      id=$i \
-      data file=$f \
-      output file=../data/meta_dump/meta_sample/zip_samp_${n}_${i}.csv &
-  done
-  wait
-  for i in `seq 1 4`;
-  do
-    # this is the poisson model
-    ./spatial_hurdle sample num_samples=5000 num_warmup=5000 random seed=420 \
-      id=$i \
-      data file=$f \
-      output file=../data/meta_dump/meta_sample/hurdle_samp_${n}_${i}.csv &
-  done
-  wait
+#  for i in `seq 1 4`;
+#  do
+#    # this is the poisson model
+#    ./spatial_zip sample num_samples=5000 num_warmup=5000 random seed=420 \
+#      id=$i \
+#      data file=$f \
+#      output file=../data/meta_dump/meta_sample/zip_samp_${n}_${i}.csv &
+#  done
+#  wait
+#  for i in `seq 1 4`;
+#  do
+#    # this is the poisson model
+#    ./spatial_hurdle sample num_samples=5000 num_warmup=5000 random seed=420 \
+#      id=$i \
+#      data file=$f \
+#      output file=../data/meta_dump/meta_sample/hurdle_samp_${n}_${i}.csv &
+#  done
+#  wait
 done
